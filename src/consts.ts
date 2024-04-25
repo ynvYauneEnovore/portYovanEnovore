6b037199f9ea44f58221aa66e60dae481d63e78e
ynvYauneEnovore <yovanuxf@gmail.com>
2024-04-25 07:43:17 -0400

    add new view

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Yovan Enovore';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const USER_NICKNAME = 'EveSunMaple';
-export const USER_DESCRIPTION = 'a student';
+export const SITE_DESCRIPTION = 'Portafolio personal de Yovan Enovore';
+export const USER_NICKNAME = 'YovanEnovore';
+export const USER_DESCRIPTION = 'Desarrollador web y diseñador gráfico';
 export const TRANSITION_API = true;
